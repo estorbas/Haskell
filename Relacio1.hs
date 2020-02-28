e9a8f11d10b41c70067729ef76d6c846acbae4cc
estorbas <45070110+estorbas@users.noreply.github.com>
2020-02-28 19:05:55 +0100

    Add files via upload
    Until exercise 7 done

--- Relacio1.hs
+++ Relacio1.hs
@@ -78,3 +78,42 @@ p4_max2 :: Ord a => a -> a -> Bool
 p4_max2 x y
   | max2 x y == y && y >= x = True
   | otherwise = False
+
+--- Ejercicio 5 ---
+
+entre :: Ord a => a -> (a,a) -> Bool
+entre x (y,z)
+    | ( x >= y && x <= z ) || (x >= z && x <= y) = True
+    | otherwise = False
+
+--- Ejercicio 6 ---
+
+iguales3 :: Eq a => (a,a,a) -> Bool
+iguales3 (x,y,z)
+  | (x == y) && (y == z) = True
+  | (x == z) && (z == y) = True
+  | (y == x) && (x == z) = True
+  | (y == z) && (z == x) = True
+  | (z == x) && (x == y) = True
+  | (z == y) && (y == x) = True
+  | otherwise = False
+
+--- Ejercicio 7 ---
+
+type TotalSegundos = Integer
+type Horas = Integer
+type Minutos = Integer
+type Segundos = Integer
+descomponer :: TotalSegundos -> (Horas,Minutos,Segundos)
+descomponer x = (horas,minutos,segundos)
+  where
+      horas = div x 3600
+      minutos = div (mod x 3600) 60
+      segundos = mod (mod x 3000) 60
+
+p_descomponer x = x>=0 ==> h*3600 + m*60 + s == x
+                           && entre m (0,59)
+                           && entre s (0,59)
+                            where (h,m,s) = descomponer x
+
+--- Ejercicio 8 ---
