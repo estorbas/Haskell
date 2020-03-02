d986756f1d527c833e51f75136a1b18842a1e019
antonio <antoniopcuma@hotmail.com>
2020-03-02 19:13:57 +0100

    Update Relacion2.hs

--- Relacion2.hs
+++ Relacion2.hs
@@ -19,9 +19,40 @@ invertirListaEficaz xs = invertir xs []
     invertir [] ys = ys
     invertir (x:xs) ys = invertir xs (x:ys)
 
+square :: Integer -> Integer
+square x = x * x
 
+-- En este caso funcionmap es una funcion de orden superior
+-- Tomara (a->b) "que es una funcion" como primer argumento
+-- y aplicara a todos los elementos de la lista la propiedad
+-- que le asignemos. En este caso square, que esta  definido
+-- arriba.
+funcionmap :: (a->b) -> [a] -> [b]
+funcionmap f []  = []
+funcionmap f (x:xs) = f x : map f xs
+
+
+filtrado :: (a -> Bool) -> [a] -> [a]
+filtrado p [] = []
+filtrado p (x:xs)
+  | p x = x : filtrado p xs
+  | otherwise = filtrado p xs
+
+--- Ejercicio 1 ---
 
 data Direction = North | South | East | West
                  deriving (Eq, Ord, Enum, Show)
 
---(<<) :: Direction -> Direction -> Bool
+(<<) :: Direction -> Direction -> Bool
+(<<) direccionA direccionB = (fromEnum direccionA) < (fromEnum direccionB)
+
+
+--- Ejercicio 2 ---
+
+maximoYresto :: Ord a => [a] -> (a,[a])
+maximoYresto  xs = masGrande xs []
+  where
+    masGrande [x] resto = (x, resto)
+    masGrande (x:y:xs) resto
+      | (y > x)  = masGrande (y:xs) (x:resto)
+      | otherwise = masGrande (x:xs) (y:resto)
